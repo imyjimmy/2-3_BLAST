b21081cc19ca2fbfcfb9310ea4b507f5fe85623f
imyjimmy-fun <imyjimmy-fun@Jimmys-MacBook-Pro.local>
2016-10-26 20:03:18 -0400

    todo: merge adjacent hits

--- Blast.java
+++ Blast.java
@@ -11,17 +11,28 @@ import java.io.FileNotFoundException;
 
 public class Blast{
   //String db_filename;
-  public static final int SLIDING_WINDOW_SIZE = 4;
+  public static int sliding_window_size = 28;
+  public double threshold = 1.0;
+
   public Hashtable<String, String> db = new Hashtable<String, String>(); //when we first process the db.
 
-  public String query;
+  public String query = "";
   public Hashtable<String, Hashtable<String, ArrayList<Integer>>> q_kmer = new Hashtable<String, Hashtable<String, ArrayList<Integer>>>();
   //                kmer1  :{ entry1 : [1 , 3 , 6, 13], entry2 : [2, 7, 14, 21]}, kmer2... 
   public Hashtable<String, Hashtable<String, ArrayList<Integer>>> db_kmer = new Hashtable<String, Hashtable<String, ArrayList<Integer>>>();
 
-
-  public double threshold = 1.0;
-
+  // public Hashtable<String, Hashtable<Integer, ArrayList<Integer>>> db_hits = new Hashtable<String, Hashtable<Integer, ArrayList<Integer>>>();
+  // db_hits
+  // Hashtable<String, HashTable<Integer, String>>
+  //           {"db1" => {3 => [12,21,45]}
+  //           { db match => { db index => [a list of lengths]}}
+  //           "db2": => {}
+  //           }
+  
+  public Hashtable<String, Hashtable<String, ArrayList<Integer>>> hits = new Hashtable<String, Hashtable<String, ArrayList<Integer>>>();
+  //              "db1" =>            {"3:12"   => [12,445,] }
+  //              db match =>         { "db_index:length" => [query_indices] }
+  
   public void parseDB(String filename) {
     // System.out.println("public void parseFile(String filename) {");
     File file = new File(filename);
@@ -46,23 +57,29 @@ public class Blast{
           } else {
             val += line;
           }
-        } while(inputFile.hasNextLine()); 
+        } while(inputFile.hasNextLine());
+
+        // System.out.println("key: " + key + " val: " + val);
+        db.put(key, val);
     } catch (FileNotFoundException fnfe) {
       fnfe.printStackTrace();
     }
 
-    System.out.println(db);
+    // System.out.println(db);
   }
 
   public void parseQuery(String filename) {
     File file = new File(filename);
     try { 
         Scanner inputFile = new Scanner(file); 
-        do {
-          this.query = inputFile.nextLine();
-          System.out.println("query: " + query);
+        while(inputFile.hasNextLine()) {
+          String line = inputFile.nextLine();
+          if (line != null) {
+            this.query += line;
+          }
           //...
-        } while(inputFile.hasNextLine()); 
+        }
+        System.out.println("query: " + query);
     } catch (FileNotFoundException fnfe) {
       fnfe.printStackTrace();
     }    
@@ -76,6 +93,8 @@ public class Blast{
       System.out.println("params: -h or --help to print this screen.");
       System.out.println("--database or -d [filepath]");
       System.out.println("--query or -q [filepath]");
+      System.out.println("--threshold or -t [1.0 or less]");
+      System.out.println("--kmer or -k [number]");
       System.out.println("command line queries are not accepted, please put in a file.");
       System.exit(0);
     } else {
@@ -86,16 +105,16 @@ public class Blast{
       b.start(parameters);  
 
       //kmerizing query string.
-      b.kmerize(b.q_kmer, SLIDING_WINDOW_SIZE, b.query, "query");
+      b.kmerize(b.q_kmer, sliding_window_size, b.query, "query");
 
-      System.out.println(b.q_kmer);
+      // System.out.println(b.q_kmer);
 
       for (String k : b.db.keySet()) {
         String val = b.db.get(k);
-        b.kmerize(b.db_kmer, SLIDING_WINDOW_SIZE, val, k);
+        b.kmerize(b.db_kmer, sliding_window_size, val, k);
       }
 
-      System.out.println(b.db_kmer);
+      // System.out.println(b.db_kmer);
 
       for (String key : b.q_kmer.keySet()) {
         b.matchAll(key, b.q_kmer.get(key).get("query"), b.db_kmer, b.threshold);  //..., b.q_kmer.get(key), ...
@@ -122,6 +141,29 @@ public class Blast{
         System.exit(1);
       }
     }
+
+    String threshold = parameters.get("-t");
+    if (threshold == null) {
+      threshold = parameters.get("--threshold");
+      if (threshold == null) {
+        System.out.println("threshold is missing, using the default 1.0");
+        threshold = "1.0";
+        // System.exit(1);
+      }
+    }
+
+    this.threshold = Double.parseDouble(threshold);
+
+    String kmer = parameters.get("-k");
+    if (kmer == null) {
+      kmer = parameters.get("--kmer");
+      if (kmer == null) {
+        System.out.println("kmer is missing, defaulting to 28");
+        kmer = "28";
+      }
+    }
+    this.sliding_window_size = Integer.parseInt(kmer);
+
     this.parseDB(db_filename);
     this.parseQuery(q_filename);
   }
@@ -152,7 +194,7 @@ public class Blast{
   
   }
 
-  //ArrayList<ArrayList<String>>
+  //return ArrayList<ArrayList<String>> (?)
   public void matchAll(String query_kmer, ArrayList<Integer> queryIndices, Hashtable<String, Hashtable<String, ArrayList<Integer>>> target, double threshold) {
     // for (String k : query.keySet()) {
     //   String val = query.get(k);
@@ -186,9 +228,7 @@ public class Blast{
   }
 
   public void extendSingleAlignment(String query_kmer, int q_index, String db_key, String db, int db_index) {
-    System.out.println("in extendSingleAlignment");
-    System.out.println("query: " + this.query + "\nquerying_kmer: " + query_kmer + "\nq_index: " + q_index + "\ndb: " + db + "\ndb_index: " + db_index);
-    
+    // System.out.println("in extendSingleAlignment");    
     //backwards
     String back_qmatch = "";
     String back_dbmatch = ""; 
@@ -203,22 +243,24 @@ public class Blast{
       } else {
         break;
       }
-      j = db_index-offset;
       offset++;
+      j = db_index-offset;
     }
-    System.out.println("back-extended:");
-    System.out.println(back_qmatch + " at: " + back_i + "\n" + back_dbmatch + " at: " + j);
+    // System.out.println("back-extended:");
+    // System.out.println(back_qmatch + " at: " + back_i + "\n" + back_dbmatch + " at: " + j);
+    int back_q_index = back_i;
+    int back_db_index = j+1;
 
     //forwards
     String forward_qmatch = "";
     String forward_dbmatch = "";
     
-    offset = 0;
+    offset = 1;
     j = db_index + query_kmer.length() + offset;
 
     int forward_i = q_index + query_kmer.length();
     for (int i = forward_i; i < this.query.length(); i++) {
-      j = db_index + query_kmer.length() + offset;
+      j = db_index + query_kmer.length() + offset - 1;
       if (j < db.length() && this.query.charAt(i) == db.charAt(j)) {
         forward_qmatch += "" + this.query.charAt(i);
         forward_dbmatch += "" + db.charAt(j);
@@ -229,15 +271,252 @@ public class Blast{
       offset++;
     }
 
-    System.out.println("forward-extended:");
-    System.out.println(forward_qmatch + " at: " + forward_i + "\n" + forward_dbmatch + " at: " + j);
+    // System.out.println("forward-extended:");
+    // System.out.println(forward_qmatch + " at: " + forward_i + "\n" + forward_dbmatch + " at: " + j);
 
     String extendedQuery = back_qmatch + query_kmer + forward_qmatch;
     String extendedDB = back_dbmatch + query_kmer + forward_dbmatch;
 
+    //todo: extend query past exact matches, up to threshold.
+    System.out.println("querying_kmer: " + query_kmer + "\nq_index: " + back_q_index + "\ndbname: " + db_key + "\ndb_index: " + back_db_index);
     System.out.println("results:");
     System.out.println(extendedQuery + "\n" + extendedDB);
+    if (! (this.threshold == 1.0)) {
+      System.out.println("extending query to threshold.");
+      extendToThreshold(extendedQuery, back_q_index, db_key, back_db_index);
+    }
 
   }
+
+  public void extendToThreshold(String match, int q_index, String db_key, int db_index) {
+    String back_qmatch = "";
+    String back_dbmatch = "";
+    boolean back_end = false;
+
+    String forward_qmatch = "";
+    String forward_dbmatch = "";
+    boolean forward_end = false;
+
+    double match_fraction = 1.0;
+    int i = 0;
+    int num_mismatches = 0;
+    int front_i = 0;
+    int back_i = 0;
+
+    String db = this.db.get(db_key);
+
+
+    while (match_fraction > this.threshold) {
+      // System.out.println("inside while");
+
+      if (i % 2 == 0 && !back_end) { //back way
+        // System.out.println("back way");
+        //q_index - 1 - i  vs db_index - 1 - i
+        if (q_index-1-back_i > -1 && db_index-1-back_i > -1) {
+          if (this.query.charAt(q_index-1-back_i) == db.charAt(db_index-1-back_i)) {
+            //nothing to actually do here
+          } else {
+            num_mismatches++;
+            match_fraction = 1.0 - ((double) num_mismatches / (double) (match.length()+forward_qmatch.length()+back_i+1));
+          }
+
+          back_qmatch = "" + this.query.charAt(q_index-1-back_i) + back_qmatch;
+          back_dbmatch = "" + db.charAt(db_index-1-back_i) + back_dbmatch;
+          back_i++;
+        } else {
+          //do not pass go, do not collect 200
+          // System.out.println("reached the back end. (start of string)");
+          back_end = true;
+        }
+
+      } else if (i % 2 == 1 && !forward_end) { //front way
+        //q_index+match.length() + i vs db_index + match.length() + i
+        // System.out.println("going front way");
+        if (q_index+match.length()+front_i < this.query.length() && db_index + match.length() + front_i < db.length()) {
+          int q = q_index+match.length()+front_i;
+          int d = db_index+match.length()+front_i;
+          if (this.query.charAt(q_index+match.length()+front_i) == db.charAt(db_index+match.length()+front_i)) {
+            //
+            // System.out.println("there was a match");
+          } else {
+            // System.out.println("mismatch");
+            // System.out.println("at query: " + q + " at db: " + d + " comparing: " + this.query.charAt(q_index+match.length()+front_i) + " with " + db.charAt(db_index+match.length()+front_i));
+            num_mismatches++;
+            match_fraction = 1.0 - ((double) num_mismatches / (double) (match.length() + back_qmatch.length() + front_i + 1));
+            // System.out.println("match_fraction: " + match_fraction);
+          }
+
+          forward_qmatch += "" + this.query.charAt(q_index+match.length()+front_i);
+          forward_dbmatch += "" + db.charAt(db_index+match.length()+front_i);
+          front_i++;
+
+          // System.out.println("query forward: " + forward_qmatch + "\ndb forward: " + forward_dbmatch);
+        } else {
+          //do not pass go...
+          // System.out.println("reached the forward end (end of string)");
+          forward_end = true;
+        }
+
+      } else {
+        if (back_end && forward_end) {
+          // System.out.println("breaking...");
+          break;
+        }
+        // System.out.println("wierd else place");
+      }
+
+      i++;
+    }
+
+    q_index = q_index - back_qmatch.length();
+    db_index = db_index - back_dbmatch.length();
+
+    String extendedQuery = back_qmatch + match + forward_qmatch;
+    String extendedDB = back_dbmatch + match + forward_dbmatch;
+
+    // System.out.println("q_index: " + q_index + "\ndbname: " + db_key + "\ndb_index: " + db_index);
+
+    // System.out.println("results:");
+    // System.out.println(extendedQuery + "\n" + extendedDB);
+
+    addHit(extendedQuery, q_index, db_key, extendedDB, db_index);
+    printConsensus(extendedQuery, q_index, db_key, extendedDB, db_index);
+  }
+
+  public void addHit(String query, int q_index, String db_key, String db_substr, int db_index) {
+    //db_hits
+    //query_hits
+    Hashtable<Integer, String> hits = db_hits.get(db_key);
+    if (hits == null) {
+      System.out.println("hits are null!");
+      hits = new Hashtable<Integer, String>(); 
+    } else {
+      System.out.println("hits are not null!");
+    }
+
+    String result = hits.get(db_index);
+    if (result == null) {
+      hits.put(db_index, db_substr);  
+    } else if (result.compareTo(db_substr) == 0 ) {
+      System.out.println("found the same string, doing nothing.");
+    } else {
+      System.out.println("not the same string, mind blown, exiting.");
+      System.out.println("db name: " + db_key + " db index:" + db_index + "\n" + result + "\n" + db_substr);
+      System.out.println("query: " + query + " q_index: " + q_index);
+      // System.exit(1);
+    }
+
+    db_hits.put(db_key, hits);
+
+    Hashtable<Integer, ArrayList<Integer>> q_hits = query_hits.get(db_key);
+    if (q_hits == null) {
+      q_hits = new Hashtable<Integer, ArrayList<Integer>>();
+    }
+
+    ArrayList<Integer> matches = q_hits.get(db_index);
+    if (matches == null) {
+      matches = new ArrayList<Integer>();
+    }
+    matches.add(q_index); //@todo, in order.
+
+    q_hits.put(db_index, matches);
+    query_hits.put(db_key, q_hits);
+  }
+
+  public void printConsensus(String query, int q_index, String db_key, String db_substr, int db_index) {
+    String buffer = "";
+    int db_index_parse = db_index;
+    while (db_index_parse / 10 != 0) {
+      buffer += " ";
+      db_index_parse = db_index_parse / 10;
+    }
+    if (query.length() < 60) {
+      System.out.print(buffer + q_index);
+      for (int i = 0; i < query.length()-2; i++) {
+        System.out.print(" ");
+      }
+      int q_end = q_index + query.length()-1;
+      System.out.println(q_end + " query");
+
+      //query string
+      System.out.println(buffer + query);
+
+      //horiz hashes
+      System.out.print(buffer);
+      for (int i = 0; i < query.length(); i++) {
+        System.out.print("|");
+      }
+      System.out.println();
+      
+      //db string
+      System.out.println(buffer+db_substr);
+
+      //db index
+      System.out.print(db_index);
+      for (int i = 0; i < db_substr.length()-2; i++) {
+        System.out.print(" ");
+      }
+      int db_end = db_index + db_substr.length() - 1;
+      System.out.println(db_end + " " + db_key);
+
+      //----//
+      System.out.print(buffer);
+      for (int i = 0; i < query.length(); i++) {
+        System.out.print("-");
+      }
+      System.out.println();
+
+      //concensus
+      System.out.print(buffer);
+      for (int i = 0; i < query.length(); i++) {
+        if (query.charAt(i) == db_substr.charAt(i)) {
+          System.out.print("*");
+        } else {
+          System.out.print("$");
+        }
+      }
+
+      System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    } else {
+
+      //query, q_index, db_key, db_substr
+      System.out.println("query match at: " + q_index + " with db: " + db_key + " at: " + db_index); 
+      int bottom_index = 0;
+      for (int i = 0; i < query.length(); i++) {             
+          System.out.print(query.charAt(i));    
+          if (((i % 60 == 0) && i > 0) || (i == query.length()-1)) {
+              System.out.print("\n");
+              for (int j = bottom_index; j<=i; j++) {
+                  System.out.print("|");
+                  if (j == i) {
+                      System.out.print("\n");
+                  }
+              }
+              for (int k = bottom_index; k<=i; k++) {
+                  System.out.print(db_substr.charAt(k));  
+              }
+              System.out.println();
+              for (int l = bottom_index; l<=i; l++) {
+                System.out.print("-");
+              }
+              System.out.println();
+              for (int m = bottom_index; m<=i; m++) {
+                if (db_substr.charAt(m) == query.charAt(m)) {
+                  System.out.print("*");
+                } else {
+                  System.out.print("$");
+                }
+                bottom_index++;
+              }
+              System.out.println("\n");
+          }
+      } 
+      System.out.print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    }
+
+  }
+
+
+
 
 }
